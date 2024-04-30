4fc63614649030c63e938c18f9d95497e2ef71e4
alicia-perez-diez <perezdiezalicia9@gmail.com>
2024-04-30 09:33:07 +0200

    V_3

--- create.sql
+++ create.sql
@@ -3,7 +3,7 @@ USE lab_mysql;
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
 id INT AUTO_INCREMENT PRIMARY KEY,
-customer_id INT NOT NULL,
+customer_id VARCHAR(20) NOT NULL,
 name VARCHAR(255) NOT NULL,
 phone_number VARCHAR(15),
 email VARCHAR(255),
@@ -25,19 +25,19 @@ color VARCHAR(50)
 DROP TABLE IF EXISTS sales;
 CREATE TABLE sales (
 id INT AUTO_INCREMENT PRIMARY KEY,
-sales_id INT NOT NULL,
+sales_id VARCHAR(20) NOT NULL,
 name VARCHAR(100),
 store VARCHAR(100)
 );
 DROP TABLE IF EXISTS invoices;
 CREATE TABLE invoices (
 id INT AUTO_INCREMENT PRIMARY KEY,
-invoice_number INT NOT NULL,
+invoice_number VARCHAR(20) NOT NULL,
 date DATE,
-customer_id INT,
-car_id INT,
-sales_id INT,
-FOREIGN KEY (car_id) REFERENCES cars(car_id),
-FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-FOREIGN KEY (sales_id) REFERENCES sales(sales_id)
+customer_id VARCHAR(20),
+car_id VARCHAR(20),
+sales_id VARCHAR(20),
+FOREIGN KEY (car_id) REFERENCES cars(id),
+FOREIGN KEY (customer_id) REFERENCES customers(id),
+FOREIGN KEY (sales_id) REFERENCES sales(id)
 );
--- update.sql
+++ update.sql
@@ -1,5 +1,7 @@
-INSERT INTO customers (name, email)
-VALUES
-	('Pablo Picasso', 'ppicasso@gmail.com'),
-    ('Abraham Lincoln', 'lincoln@us.gov'),
-    ('Napoléon Bonaparte', 'hello@napoleon.me');
+SET SQL_SAFE_UPDATES = 0;
+UPDATE customers
+SET email = 'ppicasso@gmail.com' WHERE name = 'Pablo Picasso';
+UPDATE customers
+SET email = 'lincoln@us.gov' WHERE name = 'Abraham Lincoln';
+UPDATE customers
+SET email = 'hello@napoleon.me' WHERE name = 'Napoléon Bonaparte';
